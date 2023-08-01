a6e74f84152dc9dfa9f17b04cc36d3963905f590
22souleymane <souleymane485@gmail.com>
2023-08-01 17:15:27 +0200

    first commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+      prompt: "what is 1+3?",
+      optionA: "24",
+      optionB: "21",
+      optionC: "4",
+      optionD: "2",
+      answer: "C",
+    },
+    {
+        prompt: "what is a Streaming Platform?",
+        optionA: "Twitter",
+        optionB: "Twitch",
+        optionC: "Googlw",
+        optionD: "Robinhood",
+        answer: "B",
+      },
+      {
+        prompt: "Best tech youtube channel?",
+        optionA: "PedroTech",
+        optionB: "Dev Ed",
+        optionC: "Fireship",
+        optionD: "TechwithTim",
+        answer: "A",
+      },
+];
